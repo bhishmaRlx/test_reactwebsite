ae9e38937b3f912759c240e128e9a2f50f780eaa
Florebit Digital <florebitdigital@Florebits-MacBook-Pro.local>
2020-10-06 17:47:51 +0530

    Updates

---
+++ precache-manifest.c5dfa1b8350ad55eeee3c872ff73a720.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53da6ca93a9123e77fb98b15e573b0f1",
+    "url": "/test_reactwebsite/index.html"
+  },
+  {
+    "revision": "83f73b61651bdcbb86be",
+    "url": "/test_reactwebsite/static/css/2.ea6b7726.chunk.css"
+  },
+  {
+    "revision": "174311bc1217728cece4",
+    "url": "/test_reactwebsite/static/css/main.d5d5abad.chunk.css"
+  },
+  {
+    "revision": "83f73b61651bdcbb86be",
+    "url": "/test_reactwebsite/static/js/2.88d3ba3e.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/test_reactwebsite/static/js/2.88d3ba3e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "174311bc1217728cece4",
+    "url": "/test_reactwebsite/static/js/main.89a73d6b.chunk.js"
+  },
+  {
+    "revision": "2c52496be28a61fcc44d",
+    "url": "/test_reactwebsite/static/js/runtime-main.4936868f.js"
+  },
+  {
+    "revision": "843184cec2cc55aefced958929ba6914",
+    "url": "/test_reactwebsite/static/media/about-us.843184ce.svg"
+  },
+  {
+    "revision": "45c773f3f8a21d336cfe4fe5efd24376",
+    "url": "/test_reactwebsite/static/media/home.45c773f3.svg"
+  }
+]);
